8a02a074f558a7749605126c7850b66529993231
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-03 19:31:51 +0600

    window all kind of height and width

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// select dom element
-const width = document.getElementById('width');
-const height = document.getElementById('height');
+// // select dom element
+// const width = document.getElementById('width');
+// const height = document.getElementById('height');
 
-let myWindow;
+// let myWindow;
 
-// show window object properties
-width.innerHTML = "Window inner width  is: " + window.innerWidth;
-height.innerHTML = "Window inner heitht  is: " + window.innerHeight;
+// // show window object properties
+// width.innerHTML = "Window inner width  is: " + window.innerWidth;
+// height.innerHTML = "Window inner heitht  is: " + window.innerHeight;
 
-function openWindow() {
-    myWindow =  window.open('https://www.google.com/')
-}
+// function openWindow() {
+//     myWindow =  window.open('https://www.google.com/')
+// }
 
-function closeWindow() {
-    myWindow.close();
-}
+// function closeWindow() {
+//     myWindow.close();
+// }
+
+
 
