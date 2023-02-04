c593ad4b88bf2ed53cf6e6611b4c75f1eb46c648
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-04 11:41:10 +0600

    cookie 'set, get, check'

--- script.js
+++ script.js
@@ -52,5 +52,46 @@
 // setInterval(function)
 
 
+// cookie let you store user information
+// const setNameCookie = document.querySelector('#set-name-cookie')
+// const setUsernameCookie = document.querySelector('#set-username-cookie')
+// const getNameCookie = document.querySelector('#get-name-cookie')
+// const getUsernameCookie = document.querySelector('#get-username-cookie')
+
+function setCookie(cookieName, cookieValue, exDays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exDays * 24 * 60 * 60 * 1000);
+
+    let expires = "expries=" + d.toUTCString();
+
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/"
+}
+
+function getCookie(cookieName) {
+    let cookieArray = document.cookie.split(";");
+    let name = cookieName + "=";
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let c = cookieArray[i].trim();
+
+        if (c.indexOf(name) > -1) {
+            return (c.substring(name.length));
+        }
+    }
+
+    return false;
+}
+
+function checkCookie(name) {
+    let cookieValue = getCookie(name);
+
+    if (!cookieValue) {
+        setCookie('name', 'mehedi', 2);
+    }
+    else {
+        console.log('welcome back ' + cookieValue);
+    }
+}
+
 
 
